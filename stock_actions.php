295dbd7be80e3b9d9216e9c4b6cfd61501ac87a7
Piero Coronado <pacoronado01@gmail.com>
2022-11-11 10:57:09 -0500

    Create forms for inserting and updating stock info

--- stock_actions.php
+++ stock_actions.php
@@ -21,59 +21,55 @@
       or die("<br>Cannot connect to DB:$dbname on $host, error: " . mysqli_connect_errno());
 
     $user_id = $_COOKIE['userID'];
-    echo "<h1>Manage Stock</h1>";
-    echo "<hr>";
-
-    echo "<h2>Search Stock Information</h2>";
-    echo "<form name='search_stock' action='POST'>";
 ?>
-<p> 
-Item Name: 
-<select name="item_name_search" id="">
-    <option value="*">ALL</option>
-    <?php
-    //get the item names from items table
-    $items_sql = "select itemCode, itemName from totsandblocks.Item";
-    $items_results = mysqli_query($con, $items_sql);
-
-    if($items_results){
-        while($items_row = mysqli_fetch_array($items_results)){
-            $itemCode = $items_row['itemCode'];
-            $itemName = $items_row['itemName'];
-            echo "<option value='$itemCode'>$itemName</option>";
-        }
-        mysqli_free_result($items_results);
-    } else {
-        echo "Something is wrong with SQL: " . mysqli_error($con);
-    }
-    ?>
-</select>
-</p>
-</p>
-Item Location: 
-<select name="item_location_search">
-    <option value="*">ALL</option>
-    <?php
-    //get the item names from items table
-    $locations_sql = "select locationID, locationName from totsandblocks.Location";
-    $locations_results = mysqli_query($con, $locations_sql);
-
-    if($locations_results){
-        while($location_row = mysqli_fetch_array($locations_results)){
-            $locationID = $location_row['locationID'];
-            $locationName = $location_row['locationName'];
-            echo "<option value='$locationID'>$locationName</option>";
-        }
-        mysqli_free_result($locations_results);
-    } else {
-        echo "Something is wrong with SQL: " . mysqli_error($con);
-    }
-    ?>
-</select>
-</p>
-<input type="submit" value="Search Stock">
-</form>
-<hr>
+    <a href='login.php'>Back to Homepage</a>
+    <h1>Manage Stock</h1>
+    <hr>
+    <main id="stock-main">
+        <div class="insert-initialstock-ctn">
+            <h2>Insert New item Stock Information</h2>
+            <form action="">
+                <p>Item Name:
+                    <select name="item_insert_name" required>
+                        <option value="">Select Item</option>
+                    </select>
+                </p>
+                <p> Location:
+                    <select name="item_insert_location" required>
+                        <option value="">Select Location</option>
+                    </select>
+                </p>
+                <p>Quantity:
+                    <input type="text" name="item_insert_quantity" required>
+                </p>
+                <p><input type="submit" value="Insert Item" name="btnSubmitInsert"></p>
+            </form>
+        </div>
+        <div class="update-itemquantity-ctn">
+            <form action="">
+            <p>Item Name:
+                <select name="item_update_name" required>
+                    <option value="">Select Item</option>
+                </select>
+            </p>
+            <p> Location:
+                <select name="item_update_location" required>
+                    <option value="">Select Location</option>
+                </select>
+            </p>
+            <p>Quantity:
+                <input type="text" name="item_update_quantity" required>
+            </p>
+            <p>
+                <input type="radio" id="add" value="add_quantity" name="update_quantity">
+                <label for="add">Add</label>
+                <input type="radio" id="delete" value="del_quantity" name="update_quantity">
+                <label for="delete">Add</label>
+            </p>
+            <p><input type="submit" value="Insert Item" name="btnSubmitUpdate"></p>
+            </form>
+        </div>
+    </main>
 <?php
 mysqli_close($con);
 ?>

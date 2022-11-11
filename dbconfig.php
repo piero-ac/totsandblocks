aefd0269acfff9d4f9c275120e0c6262d0b30b8f
Piero Coronado <pacoronado01@gmail.com>
2022-11-11 11:43:00 -0500

    add connection to database

--- dbconfig.php
+++ dbconfig.php
@@ -3,4 +3,9 @@ $host = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "totsandblocks";
+
+$con = new mysqli($host, $username, $password, $dbname);
+if($con->connect_error){
+    die("Connection failed: " . $con->connect_error);
+}
 ?>

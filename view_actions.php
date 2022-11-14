ac83be92b093bf5291ac61a31802b8020f49256d
Piero Coronado <pacoronado01@gmail.com>
2022-11-14 12:14:15 -0500

    Add form input for quantity filters

--- view_actions.php
+++ view_actions.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View/Print Inventory</title>
     <script>
-        function printData()
-        {
+        function printData() {
             let divToPrint = document.getElementById("printTable");
             newWin = window.open("");
             newWin.document.write(divToPrint.outerHTML);
@@ -16,16 +16,17 @@
         }
     </script>
 </head>
+
 <body>
     <?php
-        // check if user id exists
-        if(!isset($_COOKIE['userID'])) { 
-            echo "Please login first!"; 
-            die;
-        }
+    // check if user id exists
+    if (!isset($_COOKIE['userID'])) {
+        echo "Please login first!";
+        die;
+    }
 
-        $user_id = $_COOKIE['userID'];
-        require('dbfunctions.php');
+    $user_id = $_COOKIE['userID'];
+    require('dbfunctions.php');
     ?>
 
     <a href='login.php'>Back to Homepage</a>
@@ -38,7 +39,7 @@
                     <select name="item_category" id="category" required>
                         <option value="*">ALL</option>
                         <?php
-                            getCategories();
+                        getCategories();
                         ?>
                     </select>
                 </label>
@@ -46,23 +47,43 @@
                     <select name="item_location" id="location" required>
                         <option value="*">ALL</option>
                         <?php
-                            getLocations();
+                        getLocations();
                         ?>
                     </select>
                 </label>
+                <br>
+                <label>
+                    Quantity is
+                    <select name="comparison_op" required>
+                        <option value="">Select one</option>
+                        <option value="=">equal to</option>
+                        <option value="<">less than</option>
+                        <option value="<=">less than or equal to</option>
+                        <option value=">">greater than</option>
+                        <option value=">=">greater than or equal to</option>
+                    </select>
+                    <input type="number" name="quantity-filter" min="0" max="1000" required>
+                </label>
+
                 <p><input type="submit" value="Search" name="btnSearch"></p>
                 <?php
-                    if(isset($_POST['btnSearch'])){
-                        $itemCategory = $_POST['item_category'];
-                        $itemLocation = $_POST['item_location'];
-                        echo "category: $itemCategory <br>";
-                        echo "location: $itemLocation <br>";
-                        viewInventory($itemCategory, $itemLocation);
+                if (isset($_POST['btnSearch'])) {
+                    $itemCategory = $_POST['item_category'];
+                    $itemLocation = $_POST['item_location'];
+                    $quantityComparison = $_POST['comparison_op'];
+                    $quantityNumber = $_POST['quantity-filter'];
+                    // echo "category: $itemCategory <br>";
+                    // echo "location: $itemLocation <br>";
+                    // echo "comparison: $quantityComparison <br>";
+                    // echo "number: $quantityNumber <br>";
+                    $successful = viewInventory($itemCategory, $itemLocation, $quantityComparison, $quantityNumber);
+                    if ($successful)
                         echo "<br><br><button onclick=printData()>Print Table</button>";
-                    }
+                }
                 ?>
             </form>
         </div>
     </main>
 </body>
+
 </html>

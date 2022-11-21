2a651c360d20ac2180b51148566e3c3f5c6531ec
Piero Coronado <pacoronado01@gmail.com>
2022-11-21 12:07:13 -0500

    Change old function names to updated names

--- item_actions.php
+++ item_actions.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,15 +8,16 @@
     <title>Manage Items</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
     <?php
-        // check if user id exists
-        if(!isset($_COOKIE['userID'])) { 
-            echo "Please login first!"; 
-            die;
-        }
-        $user_id = $_COOKIE['userID'];
-        require('dbfunctions.php');
+    // check if user id exists
+    if (!isset($_COOKIE['userID'])) {
+        echo "Please login first!";
+        die;
+    }
+    $user_id = $_COOKIE['userID'];
+    require('dbfunctions.php');
 
     ?>
     <a href='login.php'>Back to Homepage</a>
@@ -24,18 +26,18 @@
     <main id="items-main" class="side-by-side">
         <div class="add-items-ctn">
             <h2>Add Item</h2>
-             <!-- ADD ITEMS FORM -->
-            <form name='add_item' method='POST' >
+            <!-- ADD ITEMS FORM -->
+            <form name='add_item' method='POST'>
                 <p>Item Code:
                     <input type="text" name="item_code" required>
                 </p>
-                <p>Item Name: 
+                <p>Item Name:
                     <input type="text" name="item_name" required>
                 </p>
                 <p>Item Category:
                     <select name="item_category">
                         <?php
-                            getCategories();
+                        get_item_categories();
                         ?>
                     </select>
                 </p>
@@ -43,14 +45,14 @@
                 <p><textarea name="item_comment" cols="30" rows="10"></textarea></p>
                 <p><input type="submit" value="Add Item"></p>
                 <?php
-                    if(isset($_POST['item_code'], $_POST['item_name'], $_POST['item_category'])){
-                        $itemCode = $_POST['item_code'];
-                        $itemName = $_POST['item_name'];
-                        $itemCategory = $_POST['item_category'];
-                        $itemComment = $_POST['item_comment'];
+                if (isset($_POST['item_code'], $_POST['item_name'], $_POST['item_category'])) {
+                    $itemCode = $_POST['item_code'];
+                    $itemName = $_POST['item_name'];
+                    $itemCategory = $_POST['item_category'];
+                    $itemComment = $_POST['item_comment'];
 
-                        insertItem($itemCode, $itemName, $itemCategory, $itemComment, $user_id);
-                    } 
+                    insert_item_info($itemCode, $itemName, $itemCategory, $itemComment, $user_id);
+                }
                 ?>
             </form>
         </div>
@@ -61,7 +63,7 @@
                     <select name="item_update" id="" required>
                         <option value=""></option>
                         <?php
-                            getItemNames();
+                        get_item_names();
                         ?>
                     </select>
                 </p>
@@ -72,7 +74,7 @@
                     <select name="new_item_category">
                         <option value="">Do not update</option>
                         <?php
-                            getCategories();
+                        get_item_categories();
                         ?>
                     </select>
                 </p>
@@ -80,18 +82,18 @@
                 <p><textarea name="new_item_comment" cols="30" rows="10"></textarea></p>
                 <p><input type="submit" value="Update Item" name="btnSubmitUpdate"></p>
                 <?php
-                    if(isset($_POST['btnSubmitUpdate'])){
-                        $itemCodeToUpdate = $_POST['item_update'];
-                        $newItemName = $_POST['new_item_name'];
-                        $newItemCategory = $_POST['new_item_category'];
-                        $newItemComment = $_POST['new_item_comment'];
+                if (isset($_POST['btnSubmitUpdate'])) {
+                    $itemCodeToUpdate = $_POST['item_update'];
+                    $newItemName = $_POST['new_item_name'];
+                    $newItemCategory = $_POST['new_item_category'];
+                    $newItemComment = $_POST['new_item_comment'];
 
-                        if(!empty($itemCodeToUpdate))
-                            updateItem($itemCodeToUpdate, $newItemName, $newItemCategory, $newItemComment);
-                        else 
-                            echo "Please select an item first.";
-                    }
-                    
+                    if (!empty($itemCodeToUpdate))
+                        update_item_info($itemCodeToUpdate, $newItemName, $newItemCategory, $newItemComment);
+                    else
+                        echo "Please select an item first.";
+                }
+
                 ?>
             </form>
         </div>
@@ -103,17 +105,17 @@
                     <select name="item_delete" id="">
                         <option value=""></option>
                         <?php
-                            getItemNames();
+                        get_item_names();
                         ?>
                     </select>
                 </p>
                 <p><input type="submit" value="Delete Item" name="btnSubmitDelete"></p>
-                <?php 
-                    if(isset($_POST['btnSubmitDelete'])){
-                        $itemCodeToDelete = $_POST['item_delete'];
-                        deleteItem($itemCodeToDelete);  
-                    }
-                    
+                <?php
+                if (isset($_POST['btnSubmitDelete'])) {
+                    $itemCodeToDelete = $_POST['item_delete'];
+                    delete_item_info($itemCodeToDelete);
+                }
+
                 ?>
                 </p>
             </form>
@@ -121,7 +123,8 @@
     </main>
     <hr>
     <?php
-        displayItems();
+    display_item_table();
     ?>
 </body>
+
 </html>
